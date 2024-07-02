4c079bb4a0ed197f6bc231636a8aa4ed8f7652b2
Vamshi Krishna <vamshi.qaeng@gmail.com>
2024-07-02 07:00:47 +0530

    Project added

---
+++ src/test/java/com/thetestingacademy/base/BaseTest.java
@@ -0,0 +1,45 @@
+package com.thetestingacademy.base;
+
+import com.thetestingacademy.actions.AssertActions;
+import com.thetestingacademy.endpoints.APIConstants;
+import com.thetestingacademy.modules.PayloadManager;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+    public RequestSpecification requestSpecification;
+    public AssertActions assertActions;
+    public PayloadManager payloadManager;
+    public JsonPath jsonPath;
+    public Response response;
+    public ValidatableResponse validatableResponse;
+
+    @BeforeTest
+    public void setUp(){
+        System.out.println("Before Test");
+        payloadManager = new PayloadManager();
+        assertActions = new AssertActions();
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(APIConstants.BASE_URL)
+                .addHeader("Content-Type","application/json")
+                .build().log().all();
+
+//        requestSpecification = RestAssured.
+//                given()
+//                .baseUri(APIConstants.BASE_URL)
+//                .contentType(ContentType.JSON)
+//                .log().all();
+    }
+
+    public String getToken() {
+        return null;
+    }
+
+
+
+
+}
